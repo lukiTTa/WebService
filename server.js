0b79ecc54d2ca84aecdeae252119a18ea50217a2
lukiTTa <lucasmarcelo308@gmail.com>
2022-05-10 15:03:54 -0300

    rotas

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require('express');
-const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.send('Hello from App Engine!');
 });
 
-app.get('/route', (req, res) => {
+router.get('/teste', (req, res) => {
   console.log('Requisição no endpoint route!');
   res.send('Endpoint working!');
 })
 
-// Listen to the App Engine-specified port, or 8080 otherwise
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});
